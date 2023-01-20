82cf4bedf23174c7c2260549429c2aed570ee7c0
3bdElsamea <asayed4761@gmail.com>
2023-01-20 23:35:22 +0200

    Before GUI

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# echo Welcome to our DBMS
-# PS3="Please Choose a Number: "
-# select item in "Create DB" "List DB" "Connect to DB" "Drop DB" "Exit"; do
-	dbMenu=$(whiptail --title "BDMS Menu" --fb --menu "Choose an option" 15 60 4 \
-	    "1" "Create Database" \
-	    "2" "List Databases" \
-	    "3" "Conect to Database" \
-	    "4" "Drop Database" \
-	    "5" "Exit" 3>&1 1>&2 2>&3)
-	case $dbMenu in
-	# case $REPLY in
+echo Welcome to our DBMS
+PS3="Please Choose a Number: "
+select item in "Create DB" "List DB" "Connect to DB" "Drop DB" "Exit"; do
+	# dbMenu=$(whiptail --title "BDMS Menu" --fb --menu "Choose an option" 15 60 4 \
+	#     "1" "Create Database" \
+	#     "2" "List Databases" \
+	#     "3" "Conect to Database" \
+	#     "4" "Drop Database" \
+	#     "5" "Exit" 3>&1 1>&2 2>&3)
+	# case $dbMenu in
+	case $REPLY in
 	1)
-		dbName=$(whiptail --title "Create Database" --inputbox "Enter Database Name: " 8 45 3>&1 1>&2 2>&3)
+		# dbName=$(whiptail --title "Create Database" --inputbox "Enter Database Name: " 8 45 3>&1 1>&2 2>&3)
 		./createDB.sh
 		exit
 		;;
@@ -24,7 +24,7 @@
 		exit
 		;;
 	4)
-		   dbName=$(whiptail --title "Drop Database" --inputbox "Enter Database Name: " 8 45 3>&1 1>&2 2>&3)
+		#    dbName=$(whiptail --title "Drop Database" --inputbox "Enter Database Name: " 8 45 3>&1 1>&2 2>&3)
 		./dropDB.sh
 		exit
 		;;
@@ -34,4 +34,4 @@
 
 	esac
 
-# done
+done
